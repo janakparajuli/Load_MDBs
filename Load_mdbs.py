52cc9c40963647a4f0e17e1343c5783a01f38b80
Janak Parajuli <janak.parajuli1@gmail.com>
2024-06-11 19:34:50 +0545

    code success  but layers not added

--- Load_mdbs.py
+++ Load_mdbs.py
@@ -4,60 +4,58 @@ import os
 # Define the root directory where VDC folders are located
 root_directory = r'E:\PROFESSION\GO\Chitawan_SO\BharatpurMetropolitan'
 
-# Path to the group layer template
-group_layer_template = r'E:\PROFESSION\GO\Chitawan_SO\BharatpurMetropolitan\GroupLayerTemplate.lyr'
-
 # Initialize ArcMap document
 mxd = arcpy.mapping.MapDocument(r"E:\PROFESSION\GO\Chitawan_SO\BharatpurMetropolitan\Load_mdbs.mxd")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
 
-def add_layer_to_map(df, layer_path, layer_name):
-    """Add a layer to the map document."""
+def add_layer_to_map(df, mdb_path, dataset):
+    """Add a dataset as a layer to the map document."""
     try:
-        layer = arcpy.mapping.Layer(layer_path)
+        layer_path = os.path.join(mdb_path, dataset)
+        layer_name = "{}_{}".format(os.path.basename(mdb_path).split('.')[0], dataset)
+        # Create a feature layer from the dataset
+        arcpy.MakeFeatureLayer_management(layer_path, layer_name)
+        layer = arcpy.mapping.Layer(layer_name)
         arcpy.mapping.AddLayer(df, layer, "BOTTOM")
-        print("Added {} to the map".format(layer_name))
+        print("Added {} from {}".format(dataset, mdb_path))
+        return layer
     except Exception as e:
-        print("Failed to add {}: {}".format(layer_name, str(e)))
-
-def create_and_add_group_layer(df, group_layer_name):
-    """Create and add a group layer to the map document."""
-    try:
-        group_layer = arcpy.mapping.Layer(group_layer_template)
-        group_layer.name = group_layer_name
-        arcpy.mapping.AddLayer(df, group_layer, "BOTTOM")
-        return group_layer
-    except Exception as e:
-        print("Failed to create group layer {}: {}".format(group_layer_name, str(e)))
+        print("Failed to add {} from {}: {}".format(dataset, mdb_path, str(e)))
         return None
+
+def organize_layers_into_groups(df):
+    """Organize layers into group layers based on VDC."""
+    layers = arcpy.mapping.ListLayers(mxd, "", df)
+    vdc_groups = {}
+
+    for layer in layers:
+        if not layer.isGroupLayer:
+            vdc_name = layer.name.split('_')[0]
+            if vdc_name not in vdc_groups:
+                # Create a new group layer
+                group_layer = arcpy.mapping.Layer()
+                group_layer.name = vdc_name + "_Group"
+                arcpy.mapping.AddLayer(df, group_layer, "BOTTOM")
+                vdc_groups[vdc_name] = group_layer
+            # Move the layer to the respective group layer
+            arcpy.mapping.MoveLayer(vdc_groups[vdc_name], layer, "BOTTOM")
 
 # Traverse the directory to find .mdb files and add them to the map
 for vdc_folder in os.listdir(root_directory):
     vdc_path = os.path.join(root_directory, vdc_folder)
     if os.path.isdir(vdc_path):
-        group_layer_name = vdc_folder + "_GroupLayer"
-        group_layer = create_and_add_group_layer(df, group_layer_name)
+        for ward_folder in os.listdir(vdc_path):
+            ward_path = os.path.join(vdc_path, ward_folder)
+            if os.path.isdir(ward_path):
+                for file in os.listdir(ward_path):
+                    if file.endswith('.mdb'):
+                        mdb_path = os.path.join(ward_path, file)
 
-        if group_layer:
-            for ward_folder in os.listdir(vdc_path):
-                ward_path = os.path.join(vdc_path, ward_folder)
-                if os.path.isdir(ward_path):
-                    for file in os.listdir(ward_path):
-                        if file.endswith('.mdb'):
-                            mdb_path = os.path.join(ward_path, file)
+                        # List all datasets in the .mdb file
+                        arcpy.env.workspace = mdb_path
+                        datasets = arcpy.ListFeatureClasses()
+                        for dataset in datasets:
+                            if 'parcel' in dataset.lower() or 'construction' in dataset.lower():
+                                add_layer_to_map(df, mdb_path, dataset)
 
-                            # List all datasets in the .mdb file
-                            arcpy.env.workspace = mdb_path
-                            datasets = arcpy.ListFeatureClasses()
-                            for dataset in datasets:
-                                if 'parcel' in dataset.lower() or 'construction' in dataset.lower():
-                                    layer_path = os.path.join(mdb_path, dataset)
-                                    add_layer_to_map(df, layer_path, dataset)
-                                    # Move the added layer to the group layer
-                                    layer = arcpy.mapping.ListLayers(mxd, dataset, df)[0]
-                                    arcpy.mapping.MoveLayer(group_layer, layer, "TOP")
-
-# Save the MXD document if needed
-mxd.saveACopy(r'E:\PROFESSION\GO\Chitawan_SO\BharatpurMetropolitan\Load_mdbs_saved.mxd')
-
-print("Process Completed")
+# Organize added
